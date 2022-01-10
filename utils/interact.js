fdd4c7c3a28ba76d8e364dacb5d5b45ab0e4b48d
testing32 <96968705+testing-321123@users.noreply.github.com>
2022-01-09 19:18:41 -0500

    Add files via upload

---
+++ utils/interact.js
@@ -0,0 +1,164 @@
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_API_URL);
+
+const contract = require("./abi.json");
+const contractAddress = "0x3E220d7aA726983ca00681B6f098DBA3Ba0007AC";
+const nftContract = new web3.eth.Contract(contract, contractAddress);
+
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      const obj = {
+        status: "",
+        address: addressArray[0],
+      };
+
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😞" + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href="https://metamask.io/download.html">
+              You must install MetaMask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "",
+        };
+      } else {
+        return {
+          address: "",
+          status: "😞",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😞" + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href="https://metamask.io/download.html">
+              You must install MetaMask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+// Contract Methods
+
+export const getMaxMintAmount = async () => {
+  const result = await nftContract.methods.nftPerAddressLimit().call();
+  return result;
+};
+
+export const getTotalSupply = async () => {
+  const result = await nftContract.methods.totalSupply().call();
+  return result;
+};
+
+export const getNftPrice = async () => {
+  const result = await nftContract.methods.cost().call();
+  const resultEther = web3.utils.fromWei(result, "ether");
+  return resultEther;
+};
+
+export const getSaleState = async () => {
+  const result = await nftContract.methods.status().call();
+  return result;
+};
+
+export const getNoFreeNft = async () => {
+  const resultMaxFree = await nftContract.methods.maxFree().call();
+  return resultMaxFree;
+};
+
+export const mintNFT = async (mintAmount) => {
+  if (!window.ethereum.selectedAddress) {
+    return {
+      success: false,
+      status: (
+        <p>
+          🦊 Connect to Metamask using{" "}
+          <span className="px-2 text-purple-600">Connect Wallet</span> button.
+        </p>
+      ),
+    };
+  }
+
+  const resultCost = await nftContract.methods.cost().call();
+  const resultEther = web3.utils.fromWei(resultCost, "ether");
+
+  const resultSuppy = await nftContract.methods.totalSupply().call();
+
+  const resultMaxFree = await nftContract.methods.maxFree().call();
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    value: (resultMaxFree > resultSuppy ? parseInt(web3.utils.toWei(resultEther, "ether") * mintAmount).toString(16) : parseInt(0).toString(16)), // hex
+    gasLimit: "0",
+    data: (resultMaxFree > resultSuppy ? nftContract.methods.mint(mintAmount).encodeABI() : nftContract.methods.mintfree(mintAmount).encodeABI()), //make call to NFT smart contract
+  };
+
+  //sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
+    };
+  }
+};
